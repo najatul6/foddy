992ce6cfa6fc18fd68576ecf0a4cc2c13dff5536
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-03 22:59:11 +0600

    add Navbar and Footer components to MainLayout for improved structure

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../Components/Shared/Navbar/Navbar"
+import Footer from "../Components/Shared/Footer/Footer"
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+        <Navbar/>
+        <Outlet/>
+        <Footer/>
+    </div>
   )
 }
 
