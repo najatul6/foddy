e53f099df9d2d2fb816299a9d542509e23afaacc
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 00:00:10 +0600

    feat(AboutOurFoodSection): add opening hours section to LeftSection for better information display

--- src/Components/AboutOurFoodSection/LeftSection.jsx
+++ src/Components/AboutOurFoodSection/LeftSection.jsx
@@ -6,7 +6,11 @@ const LeftSection = () => {
     <div className='w-full h-full'>
       <img src={Rectangle654} alt="" />
       <img src={Rectangle655} alt="" />
-      <img src={Rectangle654} alt="" />
+      <div>
+        <h1>OPENING HRS</h1>
+        <p>Mon - Satday | 8.00 am to 11.00 pm</p>
+        <p>Sunday | 10.00 am to 11.45 pm</p>
+      </div>
       <img src={Rectangle656} alt="" />
     </div>
   )
