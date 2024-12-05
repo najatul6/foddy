558831bc6356fab2c130cd63bee3a0f1883aa65a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 00:03:01 +0600

    feat(AboutOurFoodSection): restructure LeftSection layout and improve image organization

--- src/Components/AboutOurFoodSection/LeftSection.jsx
+++ src/Components/AboutOurFoodSection/LeftSection.jsx
@@ -1,19 +1,25 @@
-import Rectangle654 from '../../assets/resources/Rectangle 654.png'
-import Rectangle655 from '../../assets/resources/Rectangle 655.png'
-import Rectangle656 from '../../assets/resources/Rectangle 656.png'
+import Rectangle654 from "../../assets/resources/Rectangle 654.png";
+import Rectangle655 from "../../assets/resources/Rectangle 655.png";
+import Rectangle656 from "../../assets/resources/Rectangle 656.png";
 const LeftSection = () => {
   return (
-    <div className='w-full h-full'>
-      <img src={Rectangle654} alt="" />
-      <img src={Rectangle655} alt="" />
+    <div className="w-full h-full">
+      <div>
+        <img src={Rectangle654} alt="" />
+      </div>
+      <div>
+        <img src={Rectangle655} alt="" />
+      </div>
       <div>
         <h1>OPENING HRS</h1>
         <p>Mon - Satday | 8.00 am to 11.00 pm</p>
         <p>Sunday | 10.00 am to 11.45 pm</p>
       </div>
-      <img src={Rectangle656} alt="" />
+      <div>
+        <img src={Rectangle656} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftSection
+export default LeftSection;
