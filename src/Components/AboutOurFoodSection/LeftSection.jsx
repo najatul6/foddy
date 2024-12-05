b9ffb5a999aaf1132961ad55085bedcc74af7892
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 01:31:21 +0600

    feat(AboutOurFoodSection): improve layout and remove unnecessary borders in LeftSection

--- src/Components/AboutOurFoodSection/LeftSection.jsx
+++ src/Components/AboutOurFoodSection/LeftSection.jsx
@@ -3,19 +3,19 @@ import Rectangle655 from "../../assets/resources/Rectangle 655.png";
 import Rectangle656 from "../../assets/resources/Rectangle 656.png";
 const LeftSection = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center gap-6 border">
-      <div className="grid grid-cols-1 h-full gap-6 border w-full">
-        <div className="h-full w-full border">
+    <div className="w-full h-full flex justify-center items-center gap-6">
+      <div className="grid grid-cols-1 h-full gap-6 w-full">
+        <div className="h-full w-full">
         <img src={Rectangle654} alt="" className="w-full h-full" />
         </div>
 
-        <div className="font-cormorantGaramond flex flex-col border w-full h-full py-5 justify-center items-end">
+        <div className="font-cormorantGaramond flex flex-col  w-full h-full py-5 justify-center items-end">
           <h1 className="text-3xl font-normal">OPENING HRS</h1>
           <p className="font-light">Mon - Satday | 8.00 am to 11.00 pm</p>
           <p className="font-light">Sunday | 10.00 am to 11.45 pm</p>
         </div>
       </div>
-      <div className="grid grid-cols-1 h-full gap-6 border w-full">
+      <div className="grid grid-cols-1 h-full gap-6 w-full">
         <img src={Rectangle655} alt="" className="w-full " />
 
         <img src={Rectangle656} alt="" className="w-full" />
