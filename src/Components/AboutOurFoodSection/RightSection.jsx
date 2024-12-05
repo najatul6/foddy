6228e983e145462220c32d1da93c7aee4ca3da3f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 02:03:38 +0600

    feat(RightSection): add button with icon and improve layout for better readability

--- src/Components/AboutOurFoodSection/RightSection.jsx
+++ src/Components/AboutOurFoodSection/RightSection.jsx
@@ -1,3 +1,5 @@
+import { FaArrowRight } from "react-icons/fa";
+import Button from "../Shared/Button/Button";
 import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 
 const RightSection = () => {
@@ -7,8 +9,8 @@ const RightSection = () => {
         subHeading="About our food"
         heading="The Best Delicious Food Made From Us"
       />
-      <div>
-        <p>
+      <div className="space-y-5 mt-4">
+        <p className="opacity-[0.7]">
           Our menu is a celebration of flavors from around the world, crafted
           with care and creativity by our talented team of chefs. From savory
           appetizers to decadent desserts, each dish is a masterpiece
@@ -94,6 +96,9 @@ const RightSection = () => {
             </p>
           </div>
         </div>
+      </div>
+      <div>
+        <Button text="About More" icon={<FaArrowRight/>} />
       </div>
     </div>
   );
