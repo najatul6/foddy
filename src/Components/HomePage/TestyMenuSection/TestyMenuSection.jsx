3a1e58331d36e2a387982f9e5b69f59a40e9ffca
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 01:39:57 +0600

    feat(TestyMenuSection): update layout to use flexbox for menu items feat(Container): add overflow hidden to prevent content overflow

--- src/Components/HomePage/TestyMenuSection/TestyMenuSection.jsx
+++ src/Components/HomePage/TestyMenuSection/TestyMenuSection.jsx
@@ -24,7 +24,7 @@ const TestyMenuSection = () => {
                 <img src={leftSideImg} alt="Food Related Image" />
 
             {/* Menu Items */}
-            <div className="w-full">
+            <div className="w-full flex">
                 {
                     yummyMenu?.map((recipe, index) => (
                         <FoodCard key={index} name={recipe?.name} image={recipe?.image} price={recipe?.price} recipe={recipe?.recipe} />
