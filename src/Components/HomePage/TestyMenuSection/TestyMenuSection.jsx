bcfb94c429a0662e515207dffe1eda535074b293
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 23:19:34 +0600

    feat(TestyMenuSection): add structure and placeholders for menu content and images

--- src/Components/HomePage/TestyMenuSection/TestyMenuSection.jsx
+++ src/Components/HomePage/TestyMenuSection/TestyMenuSection.jsx
@@ -5,12 +5,27 @@ const TestyMenuSection = () => {
   return (
     <Container>
       <div>
+        {/* Section Header */}
         <div className="w-1/3 mx-auto text-center">
-          {" "}
           <SectionHeader
             heading="Explore The Yummy Menu"
             subHeading="Testy Menu"
           />
+        </div>
+        {/* Menu Content */}
+        <div>
+            {/* left side image  */}
+            <div>
+                <img src="" alt="" />
+            </div>
+
+            {/* Menu Items */}
+            <div></div>
+
+            {/* right side image */}
+            <div>
+                <img src="" alt="" />
+            </div>
         </div>
       </div>
     </Container>
