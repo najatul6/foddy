790e9a74b425a797191dee6738da67f3a4151efd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 02:15:54 +0600

    feat(OurBestChefSection): add padding to ChefCard description for better spacing

--- src/Components/HomePage/OurBestChefSection/ChefCard.jsx
+++ src/Components/HomePage/OurBestChefSection/ChefCard.jsx
@@ -4,7 +4,7 @@ const ChefCard = ({ chef }) => {
   return (
     <div className="rounded-lg overflow-hidden relative">
       <img src={chef.profile} alt="" className="w-full h-full rounded-lg border mx-auto" />
-      <div className="absolute bottom-0 w-full text-center bg-deep-black rounded-b-lg border border-t-0  bg-opacity-85">
+      <div className="absolute bottom-0 w-full text-center bg-deep-black rounded-b-lg border border-t-0  bg-opacity-85 pb-4">
         <h1 className="text-xl font-bold mt-5 ">{chef.name}</h1>
         <p className="text-gray-400">{chef.designation}</p>
       </div>
