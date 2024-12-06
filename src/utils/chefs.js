25be034e2134fd3b4f67465d775ac98d2950c644
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 01:59:12 +0600

    feat(chefs): add initial chefs data with profiles and designations

---
+++ src/utils/chefs.js
@@ -0,0 +1,64 @@
+import image1 from "../assets/resources/Rectangle 671.png"
+import image2 from "../assets/resources/Rectangle 638.png"
+export const chefs=[
+    {
+        id:1,
+        name:"Sara Smith",
+        designation:"Head Chef",
+        profile:`${image2}`,
+    },
+    {
+        id:2,
+        name:"John Doe",
+        designation:"Sous Chef",
+        profile:`${image1}`,
+    },
+    {
+        id:3,
+        name:"Sara Smith",
+        designation:"Head Chef",
+        profile:`${image2}`,
+    },
+    {
+        id:4,
+        name:"John Doe",
+        designation:"Sous Chef",
+        profile:`${image1}`,
+    },
+    {
+        id:5,
+        name:"Sara Smith",
+        designation:"Head Chef",
+        profile:`${image2}`,
+    },
+    {
+        id:6,
+        name:"John Doe",
+        designation:"Sous Chef",
+        profile:`${image1}`,
+    },
+    {
+        id:7,
+        name:"Sara Smith",
+        designation:"Head Chef",
+        profile:`${image2}`,
+    },
+    {
+        id:8,
+        name:"John Doe",
+        designation:"Sous Chef",
+        profile:`${image1}`,
+    },
+    {
+        id:9,
+        name:"Sara Smith",
+        designation:"Head Chef",
+        profile:`${image2}`,
+    },
+    {
+        id:10,
+        name:"John Doe",
+        designation:"Sous Chef",
+        profile:`${image1}`,
+    }
+]
