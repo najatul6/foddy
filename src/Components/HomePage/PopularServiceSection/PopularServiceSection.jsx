2116af9c32db56fa594f8bc508fd9ab0b13ed83f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 22:58:02 +0600

    feat(PopularServiceSection): restructure layout and add "Learn More" buttons to service cards

--- src/Components/HomePage/PopularServiceSection/PopularServiceSection.jsx
+++ src/Components/HomePage/PopularServiceSection/PopularServiceSection.jsx
@@ -10,51 +10,77 @@ const PopularServiceSection = () => {
   return (
     <div className="bg-white bg-opacity-[0.05] py-[120px] mt-[120px]">
       <Container>
-        <div className="flex justify-between items-center">
-          <div className="w-full">
-            <SectionHeader
-              heading="Best Category Foods Menu"
-              subHeading="Our Popular SERVICES"
-            />
-          </div>
-          <p>
-            Our menu is a celebration of flavors from around the world, crafted
-            with care and creativity by our talented team of chefs. From
-            desserts, each dish is a masterpiece
-          </p>
-        </div>
-        <div>
-          <div className="bg-deep-black p-[30px] border border-deep-orange space-y-5">
-            <img src={icon1} alt="Services icons here" />
-            <div className="flex flex-col gap-2">
-              <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">Special Foods</h6>
-              <p className="opacity-[0.7]">celebration of flavors from around the world, crafted </p>
+        <div className="flex flex-col gap-12">
+          <div className="flex justify-between items-center">
+            <div className="w-full">
+              <SectionHeader
+                heading="Best Category Foods Menu"
+                subHeading="Our Popular SERVICES"
+              />
             </div>
-            <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">Learn More <FaArrowRight/></button>
+            <p>
+              Our menu is a celebration of flavors from around the world,
+              crafted with care and creativity by our talented team of chefs.
+              From desserts, each dish is a masterpiece
+            </p>
           </div>
-          <div className="bg-deep-black p-[30px] border border-deep-orange space-y-5">
-            <img src={icon2} alt="Services icons here" />
-             <div className="flex flex-col gap-2">
-              <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">Special Foods</h6>
-              <p className="opacity-[0.7]">celebration of flavors from around the world, crafted </p>
+          <div className="flex gap-6 justify-center items-center">
+            <div className="bg-deep-black hover:bg-deep-orange duration-300 p-[30px] border border-deep-orange space-y-5">
+              <img src={icon1} alt="Services icons here" />
+              <div className="flex flex-col gap-2">
+                <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">
+                  Special Foods
+                </h6>
+                <p className="opacity-[0.7]">
+                  celebration of flavors from around the world, crafted{" "}
+                </p>
+              </div>
+              <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">
+                Learn More <FaArrowRight />
+              </button>
             </div>
-            <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">Learn More <FaArrowRight/></button>
-          </div>
-          <div className="bg-deep-black p-[30px] border border-deep-orange space-y-5">
-            <img src={icon3} alt="Services icons here" />
-             <div className="flex flex-col gap-2">
-              <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">Special Foods</h6>
-              <p className="opacity-[0.7]">celebration of flavors from around the world, crafted </p>
+            <div className="bg-deep-black hover:bg-deep-orange duration-300 p-[30px] border border-deep-orange space-y-5">
+              <img src={icon2} alt="Services icons here" />
+              <div className="flex flex-col gap-2">
+                <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">
+                  Special Foods
+                </h6>
+                <p className="opacity-[0.7]">
+                  celebration of flavors from around the world, crafted{" "}
+                </p>
+              </div>
+              <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">
+                Learn More <FaArrowRight />
+              </button>
             </div>
-            <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">Learn More <FaArrowRight/></button>
-          </div>
-          <div className="bg-deep-black p-[30px] border border-deep-orange space-y-5">
-            <img src={icon4} alt="Services icons here" />
-             <div className="flex flex-col gap-2">
-              <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">Special Foods</h6>
-              <p className="opacity-[0.7]">celebration of flavors from around the world, crafted </p>
+            <div className="bg-deep-black hover:bg-deep-orange duration-300 p-[30px] border border-deep-orange space-y-5">
+              <img src={icon3} alt="Services icons here" />
+              <div className="flex flex-col gap-2">
+                <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">
+                  Special Foods
+                </h6>
+                <p className="opacity-[0.7]">
+                  celebration of flavors from around the world, crafted{" "}
+                </p>
+              </div>
+              <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">
+                Learn More <FaArrowRight />
+              </button>
             </div>
-            <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">Learn More <FaArrowRight/></button>
+            <div className="bg-deep-black hover:bg-deep-orange duration-300 p-[30px] border border-deep-orange space-y-5">
+              <img src={icon4} alt="Services icons here" />
+              <div className="flex flex-col gap-2">
+                <h6 className="text-3xl font-cormorantGaramond font-bold capitalize">
+                  Special Foods
+                </h6>
+                <p className="opacity-[0.7]">
+                  celebration of flavors from around the world, crafted{" "}
+                </p>
+              </div>
+              <button className="flex justify-center items-center gap-2 text-xl font-cormorantGaramond">
+                Learn More <FaArrowRight />
+              </button>
+            </div>
           </div>
         </div>
       </Container>
