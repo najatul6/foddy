78e1a765bd87e28f147e6dd218e833df0615ca19
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 02:15:16 +0600

    feat(OurBestChefSection): update ChefCard image styling for improved appearance

--- src/Components/HomePage/OurBestChefSection/ChefCard.jsx
+++ src/Components/HomePage/OurBestChefSection/ChefCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const ChefCard = ({ chef }) => {
   return (
     <div className="rounded-lg overflow-hidden relative">
-      <img src={chef.profile} alt="" className="w-full h-full border mx-auto" />
+      <img src={chef.profile} alt="" className="w-full h-full rounded-lg border mx-auto" />
       <div className="absolute bottom-0 w-full text-center bg-deep-black rounded-b-lg border border-t-0  bg-opacity-85">
         <h1 className="text-xl font-bold mt-5 ">{chef.name}</h1>
         <p className="text-gray-400">{chef.designation}</p>
