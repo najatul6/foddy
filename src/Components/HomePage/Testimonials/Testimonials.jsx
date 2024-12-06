c863253f6d72a9c72e343d39e547150b5498922f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 03:35:25 +0600

    feat(Testimonials): add white prop to SectionHeader for improved text visibility

--- src/Components/HomePage/Testimonials/Testimonials.jsx
+++ src/Components/HomePage/Testimonials/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
     <div className="py-[120px] bg-deep-orange mt-[120px] flex justify-center items-center">
         <Container>
             <div>
-                <SectionHeader heading="Our Guests Share This About Our Foods." subHeading="testimonials"/>
+                <SectionHeader heading="Our Guests Share This About Our Foods." subHeading="testimonials" white={"#FFF"}/>
             </div>
         </Container>
     </div>
