65ef694c3496e7b52bec7168618bdc4dfdbc6efc
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 03:33:47 +0600

    feat(Testimonials): enhance layout with SectionHeader component for improved presentation

--- src/Components/HomePage/Testimonials/Testimonials.jsx
+++ src/Components/HomePage/Testimonials/Testimonials.jsx
@@ -1,7 +1,15 @@
+import Container from "../../Shared/Container/Container"
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader"
 
 const Testimonials = () => {
   return (
-    <div className="py-[120px] bg-deep-orange mt-[120px] flex justify-center items-center">Testimonials Coming Soon.....</div>
+    <div className="py-[120px] bg-deep-orange mt-[120px] flex justify-center items-center">
+        <Container>
+            <div>
+                <SectionHeader heading="Our Guests Share This About Our Foods." subHeading="testimonials"/>
+            </div>
+        </Container>
+    </div>
   )
 }
 
--- src/Components/Shared/SectionHeader/SectionHeader.jsx
+++ src/Components/Shared/SectionHeader/SectionHeader.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types"
 
-const SectionHeader = ({heading, subHeading}) => {
+const SectionHeader = ({heading, subHeading,white}) => {
   return (
     <>
-        <h2 className="font-semibold uppercase tracking-[4px] text-deep-orange">{subHeading}</h2>
+        <h2 className={`font-semibold uppercase tracking-[4px] ${white?"text-white":"text-deep-orange"}`}>{subHeading}</h2>
         <h1 className="text-5xl font-bold font-cormorantGaramond text-white mt-[10px]">{heading}</h1>
     </>
   )
@@ -12,6 +12,7 @@ const SectionHeader = ({heading, subHeading}) => {
 SectionHeader.propTypes = {
   heading: PropTypes.string.isRequired,
   subHeading: PropTypes.string.isRequired,
+  white:P
 }
 
 export default SectionHeader
