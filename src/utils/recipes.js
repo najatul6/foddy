594d31a44454979d5c3ac01ea90fa658eb4f4f3a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-06 23:12:08 +0600

    feat(recipes): add initial recipe data with details and images

---
+++ src/utils/recipes.js
@@ -0,0 +1,92 @@
+export const recipes=[
+    {
+        "id": 11,
+        "name": "Chicken Biryani",
+        "image": "https://cdn.dummyjson.com/recipe-images/11.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Chinese"
+        },
+        {
+        "id": 12,
+        "name": "Chicken Karahi",
+        "image": "https://cdn.dummyjson.com/recipe-images/12.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Chinese"
+        },
+        {
+        "id": 13,
+        "name": "Aloo Keema",
+        "image": "https://cdn.dummyjson.com/recipe-images/13.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Vegetable"
+        },
+        {
+        "id": 14,
+        "name": "Chapli Kebabs",
+        "image": "https://cdn.dummyjson.com/recipe-images/14.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Chinese"
+        },
+        {
+        "id": 15,
+        "name": "Saag (Spinach) with Makki di Roti",
+        "image": "https://cdn.dummyjson.com/recipe-images/15.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Fast Food"
+        },
+        {
+        "id": 16,
+        "name": "Japanese Ramen Soup",
+        "image": "https://cdn.dummyjson.com/recipe-images/16.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Bengali"
+        },
+        {
+        "id": 17,
+        "name": "Moroccan Chickpea Tagine",
+        "image": "https://cdn.dummyjson.com/recipe-images/17.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Fast Food"
+        },
+        {
+        "id": 18,
+        "name": "Korean Bibimbap",
+        "image": "https://cdn.dummyjson.com/recipe-images/18.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Italian"
+        },
+        {
+        "id": 19,
+        "name": "Greek Moussaka",
+        "image": "https://cdn.dummyjson.com/recipe-images/19.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Chinese"
+        },
+        {
+        "id": 20,
+        "name": "Butter Chicken (Murgh Makhani)",
+        "image": "https://cdn.dummyjson.com/recipe-images/20.webp",
+        "price":"$10",
+        "recipe":"Roasted Chicken, Rice, Spices and Herbs cooked together in a pot to perfection.",
+        "type":"Chinese/Vegetable/Italian/ Bengali/Fast Food",
+        "category":"Chinese"
+        }
+]
