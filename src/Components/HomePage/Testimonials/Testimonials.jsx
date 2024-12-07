12f2b61bbc7dbce622fc76decde3689d52511a9b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 20:38:08 +0600

    feat(Testimonials): integrate TestimonialCard component for enhanced testimonial display

--- src/Components/HomePage/Testimonials/Testimonials.jsx
+++ src/Components/HomePage/Testimonials/Testimonials.jsx
@@ -1,13 +1,16 @@
 import Container from "../../Shared/Container/Container"
 import SectionHeader from "../../Shared/SectionHeader/SectionHeader"
+import TestimonialCard from "./TestimonialCard"
 
 const Testimonials = () => {
   return (
     <div className="py-[120px] bg-deep-orange mt-[120px] flex justify-center items-center">
         <Container>
-            <div>
+            <div className="py-[120px]">
                 <SectionHeader heading="Our Guests Share This About Our Foods." subHeading="testimonials" white={"#FFF"}/>
-                
+                <div>
+                  <TestimonialCard />
+                </div>
             </div>
         </Container>
     </div>
