ac2d23a8a8356c47075ff70188ad6c65273c9e95
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 05:01:03 +0600

    feat(TestimonialCard): enhance layout and styling for review text and reviewer details

--- src/Components/HomePage/Testimonials/TestimonialCard.jsx
+++ src/Components/HomePage/Testimonials/TestimonialCard.jsx
@@ -7,17 +7,15 @@ const TestimonialCard = ({ review }) => {
   const { name, position, image, review: reviewText, rating } = review;
   return (
     <div>
-      <div className="">
         <div className="w-full h-full overflow-hidden">
           <img src={image} alt={name} className="w-full h-full object-cover" />
         </div>
-      </div>
-      <div className="mt-[30px]">
+      <div className="px-16 py-20 flex flex-col justify-center items-start gap-5 bg-deep-black">
         <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
-        <p className="text-white text-[18px]">&ldquo;{reviewText}&rdquo;</p>
-        <div className="">
-          <h3 className="text-white text-[24px] font-bold">{name}</h3>
-          <p className="text-white text-[18px]">{position}</p>
+        <p className="text-white text-4xl text-justify font-cormorantGaramond font-bold italic tracking-[2px]">&ldquo;{reviewText}&rdquo;</p>
+        <div className="font-cormorantGaramond capitalize">
+          <h3 className="text-white text-[48px] font-bold">{name}</h3>
+          <p className="text-white text-[24px] font-normal">{position}</p>
         </div>
       </div>
     </div>
