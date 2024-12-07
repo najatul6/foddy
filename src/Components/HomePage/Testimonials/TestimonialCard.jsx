cf3a0707812c9af34d48ee4b32d10c8ed3c1d42e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 04:51:17 +0600

    feat(TestimonialCard): replace star rating component with @smastrom/react-rating and update layout

--- src/Components/HomePage/Testimonials/TestimonialCard.jsx
+++ src/Components/HomePage/Testimonials/TestimonialCard.jsx
@@ -1,27 +1,26 @@
-import insertCss from 'insert-css'
-import { StarRating } from "react-star-rating-input"
+import { Rating } from "@smastrom/react-rating";
 
-const TestimonialCard = ({review}) => {
-    const {name, position, image, review:reviewText, rating} = review
+import "@smastrom/react-rating/style.css";
+
+const TestimonialCard = ({ review }) => {
+  const { name, position, image, review: reviewText, rating } = review;
   return (
     <div>
+      <div className="">
+        <div className="w-full h-full overflow-hidden">
+          <img src={image} alt={name} className="w-full h-full object-cover" />
+        </div>
+      </div>
+      <div className="mt-[30px]">
+        <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
+        <p className="text-white text-[18px]">{reviewText}</p>
         <div className="">
-            <div className="w-full h-full overflow-hidden">
-            <img src={image} alt={name} className="w-full h-full object-cover" />
-            </div>
-            <div className="">
-            <h3 className="text-white text-[24px] font-bold">{name}</h3>
-            <p className="text-white text-[18px]">{position}</p>
-            </div>
+          <h3 className="text-white text-[24px] font-bold">{name}</h3>
+          <p className="text-white text-[18px]">{position}</p>
         </div>
-        <div className="mt-[30px]">
-            <p className="text-white text-[18px]">{reviewText}</p>
-        </div>
-        <div className="mt-[30px] bg-deep-black border">
-        <StarRating value={4} />
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TestimonialCard
+export default TestimonialCard;
