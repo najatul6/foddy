802cc0a1ae47913aa4e4fcbbd03ff07286cd1ca3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 07:29:38 +0600

    feat(LatestBlogSection): enhance layout with Container and add image for better presentation

--- src/Components/HomePage/LatestBlogSection/LatestBlogSection.jsx
+++ src/Components/HomePage/LatestBlogSection/LatestBlogSection.jsx
@@ -1,11 +1,24 @@
+import Container from "../../Shared/Container/Container";
 import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
+import image from "../../../assets/resources/Rectangle 655.png"
 
 const LatestBlogSection = () => {
   return (
-    <div className="mt-[120px]">
-      <div >
-        <SectionHeader heading="Read Our Latest & Trending News Blogs" subHeading="Latest Blog" />
-      </div>
+    <div className="mt-[120px] py-24">
+      <Container>
+        <div className="w-1/2 mx-auto text-center border">
+          <SectionHeader
+            heading="Read Our Latest & Trending News Blogs"
+            subHeading="Latest Blog"
+          />
+        </div>
+        <div>
+            <div>
+            <img src={image} alt="latest blog" className="w-full object-cover" />
+            
+            </div>
+        </div>
+      </Container>
     </div>
   );
 };
