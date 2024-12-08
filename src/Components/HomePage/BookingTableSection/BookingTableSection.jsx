510652e89e12666077c6fec1dcf51c9fa103db21
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 06:36:18 +0600

    feat(BookingTableSection): enhance layout with Container and SectionHeader for improved structure

--- src/Components/HomePage/BookingTableSection/BookingTableSection.jsx
+++ src/Components/HomePage/BookingTableSection/BookingTableSection.jsx
@@ -1,4 +1,6 @@
 import Rectangle686 from "../../../assets/resources/Rectangle 686.png";
+import Container from "../../Shared/Container/Container";
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
 const BookingTableSection = () => {
   return (
       <div className="relative mt-[120px]">
@@ -7,7 +9,13 @@ const BookingTableSection = () => {
           alt="food background banner"
           className="w-full object-cover"
         />
-        <div className="absolute top-0 opacity-70 w-full h-full bg-deep-black z-[100]"></div>
+        <div className="absolute top-0 opacity-70 w-full mx-auto flex justify-center items-center h-full bg-deep-black z-[100]">
+            <Container>
+                <div>
+                <SectionHeader heading="Make A Reservations Table" subHeading="Booking Table" />
+                </div>
+            </Container>
+        </div>
     </div>
   );
 };
