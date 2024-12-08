c956e0d239418ada17076d6810e461e8873fd102
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 07:04:26 +0600

    feat(BookingTableSection): add reservation inputs and description for enhanced user interaction

--- src/Components/HomePage/BookingTableSection/BookingTableSection.jsx
+++ src/Components/HomePage/BookingTableSection/BookingTableSection.jsx
@@ -1,22 +1,67 @@
+import { FaCalendarAlt, FaClock } from "react-icons/fa";
 import Rectangle686 from "../../../assets/resources/Rectangle 686.png";
 import Container from "../../Shared/Container/Container";
 import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
+import { FaPerson } from "react-icons/fa6";
 const BookingTableSection = () => {
   return (
-      <div className="relative mt-[120px]">
-        <img
-          src={Rectangle686}
-          alt="food background banner"
-          className="w-full object-cover"
-        />
-        <div className="absolute top-0 opacity-70 w-full mx-auto flex justify-center items-center h-full bg-deep-black z-[100]">
-            <Container>
-                <div className="text-center">
-                <SectionHeader heading="Make A Reservations Table" subHeading="Booking Table" />
-                </div>
-                
-            </Container>
-        </div>
+    <div className="relative mt-[120px]">
+      <img
+        src={Rectangle686}
+        alt="food background banner"
+        className="w-full object-cover"
+      />
+      <div className="absolute top-0 opacity-80 w-full mx-auto flex justify-center items-center h-full bg-deep-black z-[100]">
+        <Container>
+          <div className="text-center">
+            <SectionHeader
+              heading="Make A Reservations Table"
+              subHeading="Booking Table"
+            />
+          </div>
+          <p className="text-center w-2/3 mx-auto opacity-[0.6]">
+            To make a reservation at our restaurant, simply call our booking
+            hotline or visit our website
+          </p>
+          <div>
+            {/* Reserve Date  */}
+            <div className="relative">
+              <input
+                type="text"
+                className="text-white w-full bg-transparent border-white border px-4 py-2 peer focus:outline-none  placeholder:text-white"
+                defaultValue={new Date().toLocaleDateString()}
+                placeholder=" Reserved Date"
+              />
+              <p className="absolute right-4 top-[10px]">
+                <FaCalendarAlt />
+              </p>
+            </div>
+            {/* Reserve Time */}
+            <div className="relative">
+              <input
+                type="text"
+                className="text-white w-full bg-transparent border-white border px-4 py-2 peer focus:outline-none  placeholder:text-white"
+                defaultValue="12:00 PM"
+                placeholder="Reserved Time"
+              />
+              <p className="absolute right-4 top-[10px]">
+                <FaClock />
+              </p>
+            </div>
+            {/* Reserve Person */}
+            <div className="relative">
+              <input
+                type="text"
+                className="text-white w-full bg-transparent border-white border px-4 py-2 peer focus:outline-none  placeholder:text-white"
+                placeholder="Person"
+              />
+              <p className="absolute right-4 top-[10px]">
+                <FaPerson />
+              </p>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed top-0 left-0 w-full py-[1.3rem] px-[2%] lg:py-1 lg:px-[2%] flex gap-1 lg:justify-between items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-deep-black  before:z-[-1] shadow-xl shadow-gradient-back text-white font-bold">
+    <header className="fixed top-0 left-0 w-full z-[999] py-[1.3rem] px-[2%] lg:py-1 lg:px-[2%] flex gap-1 lg:justify-between items-center before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-deep-black  before:z-[-1] shadow-xl shadow-gradient-back text-white font-bold">
       <div className="text-3xl font-bold lg:leading-[70px] cursor-pointer">
         <a href="/" className="block">
         F<span className="text-deep-orange">odd</span>y
