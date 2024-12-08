d7903bb9e90cac05da5a6c05c175a4fd614b9ea3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 07:52:16 +0600

    feat(Button): enhance button styling with border and hover effects for improved interactivity

--- src/Components/Shared/Button/Button.jsx
+++ src/Components/Shared/Button/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Button = ({ text, icon }) => {
   return (
-    <button className="py-2 md:py-4 px-2 md:px-6 uppercase bg-deep-orange text-white font-cormorantGaramond flex justify-center items-center gap-1">
+    <button className="py-2 md:py-4 px-2 md:px-6 uppercase bg-deep-orange border hover:bg-transparent hover:border border-deep-orange hover:text-deep-orange duration-300 text-white font-cormorantGaramond flex justify-center items-center gap-1">
       {text} {icon}
     </button>
   );
