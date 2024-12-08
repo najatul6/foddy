d75e1af1c3d4e9ff136ec962211a95d73b3082ce
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 07:47:28 +0600

    feat(LatestBlogSection): update blog card layout to grid structure with improved image sizes and spacing for better presentation

--- src/Components/HomePage/LatestBlogSection/LatestBlogSection.jsx
+++ src/Components/HomePage/LatestBlogSection/LatestBlogSection.jsx
@@ -7,61 +7,65 @@ const LatestBlogSection = () => {
   return (
     <div className="mt-[120px] py-24">
       <Container>
-        <div className="w-1/2 mx-auto text-center border">
-          <SectionHeader
-            heading="Read Our Latest & Trending News Blogs"
-            subHeading="Latest Blog"
-          />
-        </div>
         <div>
-            {/* card 1 */}
-          <div className="flex gap-6 justify-center items-center">
-            <img
-              src={image}
-              alt="latest blog"
-              className="w-full object-cover"
+          <div className="w-1/2 mx-auto text-center border">
+            <SectionHeader
+              heading="Read Our Latest & Trending News Blogs"
+              subHeading="Latest Blog"
             />
-            <div className="flex flex-col justify-start items-start gap-6 h-full">
-              <Button text="RECEPIES" />
-              <h2 className="text-[28px] font-cormorantGaramond font-bold uppercase underline">The Art of Food Pairing to Perfect Your Palate Guide</h2>
-              <p className="opacity-[0.8]">
-                Majority have suffered alteration in some form Marketing System
-              </p>
-              <div className="flex justify-start items-center gap-6">
-                <img
-                  src={image}
-                  alt="profile pic"
-                  className="w-12 h-12 border-2 rounded-full"
-                />
-                <p>David Martin</p>
-              </div>
-              <hr className="my-6 w-full opacity-[0.5]" />
-              <p className="uppercase opacity-[0.8]">April 16,2024 / Recipes</p>
-            </div>
           </div>
-            {/* card 2 */}
-          <div className="flex gap-6 justify-center items-center">
-            <img
-              src={image}
-              alt="latest blog"
-              className="w-full object-cover"
-            />
-            <div className="flex flex-col justify-start items-start gap-6 h-full">
-              <Button text="RECEPIES" />
-              <h2 className="text-[28px] font-cormorantGaramond font-bold uppercase underline">The Art of Food Pairing to Perfect Your Palate Guide</h2>
-              <p className="opacity-[0.8]">
-                Majority have suffered alteration in some form Marketing System
-              </p>
-              <div className="flex justify-start items-center gap-6">
-                <img
-                  src={image}
-                  alt="profile pic"
-                  className="w-12 h-12 border-2 rounded-full"
-                />
-                <p>David Martin</p>
+          <div className="grid grid-cols-2 gap-6">
+            {/* card 1 */}
+            <div className="flex gap-6 justify-center items-center">
+              <img src={image} alt="latest blog" className="w-[300px] h-[350px]" />
+              <div className="flex flex-col justify-center items-start gap-5 h-full">
+                <Button text="RECEPIES" />
+                <h2 className="text-[20px] font-cormorantGaramond font-bold uppercase underline">
+                  The Art of Food Pairing to Perfect Your Palate Guide
+                </h2>
+                <p className="opacity-[0.8]">
+                  Majority have suffered alteration in some form Marketing
+                  System
+                </p>
+                <div className="flex justify-start items-center gap-6">
+                  <img
+                    src={image}
+                    alt="profile pic"
+                    className="w-12 h-12 border-2 rounded-full"
+                  />
+                  <p>David Martin</p>
+                </div>
+                <hr className=" w-full opacity-[0.5]" />
+                <p className="uppercase opacity-[0.8]">
+                  April 16,2024 / Recipes
+                </p>
               </div>
-              <hr className="my-6 w-full opacity-[0.5]" />
-              <p className="uppercase opacity-[0.8]">April 16,2024 / Recipes</p>
+            </div>
+            {/* card 2 */}
+            <div className="flex gap-6 justify-center items-center">
+              <img src={image} alt="latest blog" className="w-[300px] h-[350px]" />
+              <div className="flex flex-col justify-center items-start gap-5 h-full">
+                <Button text="RECEPIES" />
+                <h2 className="text-[20px] font-cormorantGaramond font-bold uppercase underline">
+                  The Art of Food Pairing to Perfect Your Palate Guide
+                </h2>
+                <p className="opacity-[0.8]">
+                  Majority have suffered alteration in some form Marketing
+                  System
+                </p>
+                <div className="flex justify-start items-center gap-6">
+                  <img
+                    src={image}
+                    alt="profile pic"
+                    className="w-12 h-12 border-2 rounded-full"
+                  />
+                  <p>David Martin</p>
+                </div>
+                <hr className="w-full opacity-[0.5]" />
+                <p className="uppercase opacity-[0.8]">
+                  April 16,2024 / Recipes
+                </p>
+              </div>
             </div>
           </div>
         </div>
