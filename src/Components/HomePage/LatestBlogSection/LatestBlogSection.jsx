d5ea0ebf3d0a67030e742e120cf073730a61e51f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-08 07:25:01 +0600

    feat(LatestBlogSection): add SectionHeader for improved blog visibility

--- src/Components/HomePage/LatestBlogSection/LatestBlogSection.jsx
+++ src/Components/HomePage/LatestBlogSection/LatestBlogSection.jsx
@@ -1,8 +1,13 @@
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
 
 const LatestBlogSection = () => {
   return (
-    <div>LatestBlogSection</div>
-  )
-}
+    <div className="mt-[120px]">
+      <div >
+        <SectionHeader heading="Read Our Latest & Trending News Blogs" subHeading="Latest Blog" />
+      </div>
+    </div>
+  );
+};
 
-export default LatestBlogSection
+export default LatestBlogSection;
