aea38794dcd4156a9d85b8b7682a1a2d17320d15
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-09 23:12:08 +0600

    feat(About): integrate Banner component into About page

--- src/Components/Shared/Banner/Banner.jsx
+++ src/Components/Shared/Banner/Banner.jsx
@@ -1,7 +1,16 @@
-
+import image1 from '../../../assets/resources/Rectangle 560.png'
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="">
+        <div>
+            <img src={image1} alt="Food banner" className="w-full object-cover" />
+            <div>
+                <h1>An Epicurean Adventure Awaits Reserve Your Table Today</h1>
+                <p>Our menu is a celebration of flavors from around the world, crafted with care and creativity by our talented team of chefs. From savory appetizers to decadent desserts, each dish is a masterpiece</p>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,10 @@
+import Banner from "../../Components/Shared/Banner/Banner"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Banner/>
+    </div>
   )
 }
 
