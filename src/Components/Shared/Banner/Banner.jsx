b266ab4c61f6c5918ae5b36c8174477fbb9d9bdb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-09 23:31:34 +0600

    feat(Banner): make Banner component dynamic with heading and subHeading props

--- src/Components/Shared/Banner/Banner.jsx
+++ src/Components/Shared/Banner/Banner.jsx
@@ -1,26 +1,24 @@
 import { FaArrowRight } from "react-icons/fa";
 import image1 from "../../../assets/resources/Rectangle 560.png";
 import Button from "../Button/Button";
-const Banner = () => {
+const Banner = ({heading,subHeading}) => {
   return (
-    <div className="">
+    <>
       <div className="relative">
         <img src={image1} alt="Food banner" className="w-full object-cover" />
         <div className="absolute inset-0 text-white flex flex-col justify-center items-center text-center w-4/5 mx-auto gap-6">
           <h1 className="text-7xl font-cormorantGaramond">
-            An Epicurean Adventure Awaits Reserve Your Table Today
+            {heading}
           </h1>
           <p className="opacity-[0.9]">
-            Our menu is a celebration of flavors from around the world, crafted
-            with care and creativity by our talented team of chefs. From savory
-            appetizers to decadent desserts, each dish is a masterpiece
+            {subHeading}
           </p>
           <div className="flex justify-center items-center">
             <Button text="View Our Menu" icon={<FaArrowRight />} />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,16 @@
-import Banner from "../../Components/Shared/Banner/Banner"
+import Banner from "../../Components/Shared/Banner/Banner";
 
 const About = () => {
   return (
     <div>
-      <Banner/>
+      <Banner
+        heading={"An Epicurean Adventure Awaits Reserve Your Table Today"}
+        subHeading={
+          "Our menu is a celebration of flavors from around the world, crafted with care and creativity by our talented team of chefs. From savory appetizers to decadent desserts, each dish is a masterpiece"
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
