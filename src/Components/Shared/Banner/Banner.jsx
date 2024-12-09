96333a1e968092879ceec96288b36812ffcf0629
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-09 23:27:07 +0600

    feat(Banner): enhance Banner component with button and improved layout

--- src/Components/Shared/Banner/Banner.jsx
+++ src/Components/Shared/Banner/Banner.jsx
@@ -1,17 +1,27 @@
-import image1 from '../../../assets/resources/Rectangle 560.png'
+import { FaArrowRight } from "react-icons/fa";
+import image1 from "../../../assets/resources/Rectangle 560.png";
+import Button from "../Button/Button";
 const Banner = () => {
   return (
     <div className="">
-        <div>
-            <img src={image1} alt="Food banner" className="w-full object-cover" />
-            <div>
-                <h1>An Epicurean Adventure Awaits Reserve Your Table Today</h1>
-                <p>Our menu is a celebration of flavors from around the world, crafted with care and creativity by our talented team of chefs. From savory appetizers to decadent desserts, each dish is a masterpiece</p>
-                
-            </div>
+      <div className="relative">
+        <img src={image1} alt="Food banner" className="w-full object-cover" />
+        <div className="absolute inset-0 text-white flex flex-col justify-center items-center text-center w-4/5 mx-auto">
+          <h1 className="text-7xl font-cormorantGaramond">
+            An Epicurean Adventure Awaits Reserve Your Table Today
+          </h1>
+          <p className="opacity-[0.9]">
+            Our menu is a celebration of flavors from around the world, crafted
+            with care and creativity by our talented team of chefs. From savory
+            appetizers to decadent desserts, each dish is a masterpiece
+          </p>
+          <div className="flex justify-center items-center">
+            <Button text="View Our Menu" icon={<FaArrowRight />} />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
