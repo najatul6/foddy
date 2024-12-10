8b91ebbaf55527638f3855eb8d0ac805f36713f9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-10 23:16:34 +0600

    feat(AboutOurFood): add SectionHeader component to AboutOurFood section

--- src/Components/AboutPage/AboutOurFood/AboutOurFood.jsx
+++ src/Components/AboutPage/AboutOurFood/AboutOurFood.jsx
@@ -1,11 +1,15 @@
 import image1 from "../../../assets/resources/Rectangle 632.png"
 import image2 from "../../../assets/resources/Rectangle 631.png"
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader"
 const AboutOurFood = () => {
   return (
     <div>
         <img src={image1} alt="Food banner" className="w-full" />
         <div>
           <img src={image2} alt="Chef with Food banner" className="w-full" />
+          <div>
+            <SectionHeader heading="" subHeading="About Our Food"/>
+          </div>
         </div>
     </div>
   )
