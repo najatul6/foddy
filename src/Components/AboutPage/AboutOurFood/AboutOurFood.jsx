7ed5750fd1937ce1cc1a596e439403fa4df557e1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-10 23:19:41 +0600

    feat(AboutOurFood): enhance layout with additional images, button, and contact information

--- src/Components/AboutPage/AboutOurFood/AboutOurFood.jsx
+++ src/Components/AboutPage/AboutOurFood/AboutOurFood.jsx
@@ -1,18 +1,41 @@
-import image1 from "../../../assets/resources/Rectangle 632.png"
-import image2 from "../../../assets/resources/Rectangle 631.png"
-import SectionHeader from "../../Shared/SectionHeader/SectionHeader"
+import image1 from "../../../assets/resources/Rectangle 632.png";
+import image2 from "../../../assets/resources/Rectangle 631.png";
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
+import Button from "../../Shared/Button/Button";
+import { FaArrowRight } from "react-icons/fa";
+import { MdPhoneCallback } from "react-icons/md";
 const AboutOurFood = () => {
   return (
     <div>
-        <img src={image1} alt="Food banner" className="w-full" />
+      <img src={image1} alt="Food banner" className="w-full" />
+      <div>
+        <img src={image2} alt="Chef with Food banner" className="w-full" />
         <div>
-          <img src={image2} alt="Chef with Food banner" className="w-full" />
+          <SectionHeader
+            heading="The Best Delicious Food Made From Us"
+            subHeading="About Our Food"
+          />
+          <p>
+            Our menu is a celebration of flavors from around the world, crafted
+            with care and creativity by our talented team of chefs. From savory
+            appetizers to decadent desserts, each dish is a masterpiece
+          </p>
           <div>
-            <SectionHeader heading="The Best Delicious Food Made From Us" subHeading="About Our Food"/>
+            <Button text="About More" icon={<FaArrowRight />} />
+            <div>
+              <div>
+                <MdPhoneCallback />
+              </div>
+              <div>
+                <p>Call For Order</p>
+                <h6>+8801773827414</h6>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutOurFood
+export default AboutOurFood;
