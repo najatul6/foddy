3ec9340ad105425179cbee928c9a265e247365e5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-10 23:15:37 +0600

    feat(AboutOurFood): update image source for additional food banner

--- src/Components/AboutPage/AboutOurFood/AboutOurFood.jsx
+++ src/Components/AboutPage/AboutOurFood/AboutOurFood.jsx
@@ -1,11 +1,11 @@
 import image1 from "../../../assets/resources/Rectangle 632.png"
-import image2 from "../../"
+import image2 from "../../../assets/resources/Rectangle 631.png"
 const AboutOurFood = () => {
   return (
     <div>
         <img src={image1} alt="Food banner" className="w-full" />
         <div>
-          <img src="" alt="" />
+          <img src={image2} alt="Chef with Food banner" className="w-full" />
         </div>
     </div>
   )
