9c49c08499383276d52b84eeeb4c3bb04348ce2a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-15 23:08:30 +0600

    feat(OurBestChefs): enhance layout and styling for improved content presentation

--- src/Components/AboutPage/OurBestChefs/OurBestChefs.jsx
+++ src/Components/AboutPage/OurBestChefs/OurBestChefs.jsx
@@ -13,18 +13,24 @@ const OurBestChefs = () => {
               subHeading={"OUR best chef’s"}
             />
           </div>
-          <p>
+          <p className="text-white ">
             The format typically involves contestants facing a series of
             challenges, including mystery box challenges, invention tests, team
             challenges, and elimination rounds. They are judged by a panel of
             professional.
           </p>
-          <div>
-            <p className="font-cormorantGaramond flex justify-center items-center gap-[10px]">
+          <div className="flex flex-col justify-start items-start gap-5 text-white text-2xl relative">
+            {/* Border Styling */}
+            <div className="absolute inset-0 border-l-4 border-white"></div>
+            <div className="absolute top-0 left-0 w-1/5 border-t-4 border-white"></div>
+            <div className="absolute bottom-0 left-0 w-1/5 border-b-4 border-white"></div>
+
+            {/* Content */}
+            <p className="font-cormorantGaramond flex justify-center items-center gap-[10px] relative z-10">
               <IoIosCheckmarkCircle /> 15 years Experience in Restaurant
               Chef&rsquo;s in London
             </p>
-            <p className="font-cormorantGaramond flex justify-center items-center gap-[10px]">
+            <p className="font-cormorantGaramond flex justify-center items-center gap-[10px] relative z-10">
               <IoIosCheckmarkCircle /> Any Kind Of Food Made Yummy & Testy Food
             </p>
           </div>
