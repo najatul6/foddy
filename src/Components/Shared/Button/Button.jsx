ea960b9eb5a4481514dee4eececcc84669e5a28a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-15 23:38:00 +0600

    feat(Button): update button component to improve background styling options and text color on hover

--- src/Components/Shared/Button/Button.jsx
+++ src/Components/Shared/Button/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Button = ({ text, icon,withBg,withOutbg }) => {
   return (
-    <button className={`py-2 md:py-4 px-2 md:px-6 uppercase ${withbg?"bg-deep-orange hover:bg-transparent hover:border border-deep-orange hover:text-deep-orange": withOutbg && "bg-transparent border-white hover:bg-deep-orange hover:border-deep-orange"} border  duration-300 text-white font-cormorantGaramond flex justify-center items-center gap-1`}>
+    <button className={`py-2 md:py-4 px-2 md:px-6 uppercase ${withBg?"bg-deep-orange hover:bg-transparent hover:border border-deep-orange hover:text-deep-orange": withOutbg && "bg-transparent border-white hover:bg-deep-orange hover:border-deep-orange text-deep-orange hover:text-white"} border  duration-300 text-white font-cormorantGaramond flex justify-center items-center gap-1`}>
       {text} {icon}
     </button>
   );
