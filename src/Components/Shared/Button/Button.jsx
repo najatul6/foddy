9154e7ce519435838c4865c3660bebdff0c3062c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-15 23:36:53 +0600

    feat(Button): add conditional background styling options for improved button customization

--- src/Components/Shared/Button/Button.jsx
+++ src/Components/Shared/Button/Button.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
-const Button = ({ text, icon }) => {
+const Button = ({ text, icon,withBg,withOutbg }) => {
   return (
-    <button className="py-2 md:py-4 px-2 md:px-6 uppercase bg-deep-orange border hover:bg-transparent hover:border border-deep-orange hover:text-deep-orange duration-300 text-white font-cormorantGaramond flex justify-center items-center gap-1">
+    <button className={`py-2 md:py-4 px-2 md:px-6 uppercase ${withbg?"bg-deep-orange hover:bg-transparent hover:border border-deep-orange hover:text-deep-orange": withOutbg && "bg-transparent border-white hover:bg-deep-orange hover:border-deep-orange"} border  duration-300 text-white font-cormorantGaramond flex justify-center items-center gap-1`}>
       {text} {icon}
     </button>
   );
