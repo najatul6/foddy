6ede357d7e2c89cb4d4bccdbf166963f846c9422
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-15 23:01:19 +0600

    feat(OurBestChefs): add experience highlights and improve content formatting

--- src/Components/AboutPage/OurBestChefs/OurBestChefs.jsx
+++ src/Components/AboutPage/OurBestChefs/OurBestChefs.jsx
@@ -1,3 +1,4 @@
+import { IoIosCheckmarkCircle } from "react-icons/io";
 import Container from "../../Shared/Container/Container";
 import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
 
@@ -6,10 +7,27 @@ const OurBestChefs = () => {
     <Container>
       <div>
         <div>
-            <div  className="w-1/3 text-left">
-                <SectionHeader heading={"Meet Our Exclusive & Master Chef’s"} subHeading={"OUR best chef’s"}/>
-            </div>
-            <p>The format typically involves contestants facing a series of challenges, including mystery box challenges, invention tests, team challenges, and elimination rounds. They are judged by a panel of professional.</p>
+          <div className="w-1/3 text-left">
+            <SectionHeader
+              heading={"Meet Our Exclusive & Master Chef’s"}
+              subHeading={"OUR best chef’s"}
+            />
+          </div>
+          <p>
+            The format typically involves contestants facing a series of
+            challenges, including mystery box challenges, invention tests, team
+            challenges, and elimination rounds. They are judged by a panel of
+            professional.
+          </p>
+          <div>
+            <p>
+              <IoIosCheckmarkCircle /> 15 years Experience in Restaurant
+              Chef&rsquo;s in London
+            </p>
+            <p>
+              <IoIosCheckmarkCircle /> Any Kind Of Food Made Yummy & Testy Food
+            </p>
+          </div>
         </div>
         <div></div>
       </div>
