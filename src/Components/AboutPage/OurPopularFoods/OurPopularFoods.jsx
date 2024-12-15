0bcd6d0920ec9cfece6e434ecd6c7fd7a0302a2a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-15 21:54:42 +0600

    feat(OurPopularFoods): fetch and display popular foods from JSON data

--- src/Components/AboutPage/OurPopularFoods/OurPopularFoods.jsx
+++ src/Components/AboutPage/OurPopularFoods/OurPopularFoods.jsx
@@ -1,19 +1,28 @@
-import Container from "../../Shared/Container/Container"
-import SectionHeader from "../../Shared/SectionHeader/SectionHeader"
+import { useState } from "react";
+import Container from "../../Shared/Container/Container";
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
+import { useEffect } from "react";
 
 const OurPopularFoods = () => {
+  const [popularFoods, setPopularFoods] = useState([]);
+  useEffect(() => {
+    fetch("/popularMenu.json")
+      .then((response) => response.json())
+      .then((data) => setPopularFoods(data));
+  }, []);
   return (
     <div className="py-[120px] bg-white bg-opacity-[0.05] mt-[120px]">
-        <Container>
-            <div>
-                <SectionHeader heading="Best Category Foods Menu" subHeading="Our Popular Foods" />
-            </div>
-            <div>
-                
-            </div>
-        </Container>
+      <Container>
+        <div>
+          <SectionHeader
+            heading="Best Category Foods Menu"
+            subHeading="Our Popular Foods"
+          />
+        </div>
+        <div>{popularFoods?.length}</div>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default OurPopularFoods
+export default OurPopularFoods;
