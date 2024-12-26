9dd60484b173074b4e22d13487993e955c1a8c24
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-26 23:03:56 +0600

    fix: wrap Banner content in Container component for improved layout

--- src/Components/Shared/Banner/Banner.jsx
+++ src/Components/Shared/Banner/Banner.jsx
@@ -2,21 +2,20 @@ import { FaArrowRight } from "react-icons/fa";
 import image1 from "../../../assets/resources/Rectangle 560.png";
 import Button from "../Button/Button";
 import PropTypes from "prop-types";
-const Banner = ({heading,subHeading}) => {
+import Container from "../Container/Container";
+const Banner = ({ heading, subHeading }) => {
   return (
     <>
       <div className="relative">
         <img src={image1} alt="Food banner" className="w-full object-cover" />
         <div className="absolute inset-0 text-white flex flex-col justify-center items-center text-center w-4/5 mx-auto gap-6">
-          <h1 className="text-7xl font-cormorantGaramond">
-            {heading}
-          </h1>
-          <p className="opacity-[0.9]">
-            {subHeading}
-          </p>
-          <div className="flex justify-center items-center">
-            <Button text="View Our Menu" icon={<FaArrowRight />} withBg />
-          </div>
+          <Container>
+            <h1 className="text-7xl font-cormorantGaramond">{heading}</h1>
+            <p className="opacity-[0.9]">{subHeading}</p>
+            <div className="flex justify-center items-center">
+              <Button text="View Our Menu" icon={<FaArrowRight />} withBg />
+            </div>
+          </Container>
         </div>
       </div>
     </>
@@ -24,8 +23,8 @@ const Banner = ({heading,subHeading}) => {
 };
 
 Banner.propTypes = {
-    heading: PropTypes.string,
-    subHeading: PropTypes.string,
+  heading: PropTypes.string,
+  subHeading: PropTypes.string,
 };
 
 export default Banner;
