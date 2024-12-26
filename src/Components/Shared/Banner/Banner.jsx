8bb8dd81bb8ba0c5c52b807ce5df811fd64a92eb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-26 23:05:13 +0600

    fix: wrap Banner content in additional div for improved layout

--- src/Components/Shared/Banner/Banner.jsx
+++ src/Components/Shared/Banner/Banner.jsx
@@ -8,13 +8,15 @@ const Banner = ({ heading, subHeading }) => {
     <>
       <div className="relative">
         <img src={image1} alt="Food banner" className="w-full object-cover" />
-        <div className="absolute inset-0 text-white flex flex-col justify-center items-center text-center w-4/5 mx-auto gap-6">
+        <div className="absolute inset-0 ">
           <Container>
-            <h1 className="text-7xl font-cormorantGaramond">{heading}</h1>
+           <div className="text-white flex flex-col justify-center items-center text-center w-4/5 mx-auto gap-6">
+           <h1 className="text-7xl font-cormorantGaramond">{heading}</h1>
             <p className="opacity-[0.9]">{subHeading}</p>
             <div className="flex justify-center items-center">
               <Button text="View Our Menu" icon={<FaArrowRight />} withBg />
             </div>
+           </div>
           </Container>
         </div>
       </div>
