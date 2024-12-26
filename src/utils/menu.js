201eef0f5eba81835ef39569411d163747829eac
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-26 22:28:31 +0600

    fix: update route IDs to ensure unique string format

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,7 +1,7 @@
 export const routes = [
     { id: "dkoeeee_ierj21", path: "/", name: "Home", component: "Home" },
-    { id: "dkoeeee_iereew21", path: "/about", name: "About", component: "About" },
-    { id: "dkoeeee_ierrf3j21", path: "/contact", name: "Contact", component: "Contact" },
-    { id: "dkoeeee_ier23j21", path: "/services", name: "Services", component: "Services" },
-    { id: "dkoeeee_iere3rj21", path: "/profile/:username", name: "Profile", component: "Profile" },
+    { id: "dkoe&eee_iereew21", path: "/about", name: "About", component: "About" },
+    { id: "dko(eeee_ierrf3j21", path: "/contact", name: "Contact", component: "Contact" },
+    { id: "dko*eeee_ier23j21", path: "/services", name: "Services", component: "Services" },
+    { id: "dko-eeee_iere3rj21", path: "/profile/:username", name: "Profile", component: "Profile" },
   ];
