09610e22b01667e4ebc39ac361311979436e8c3a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-26 23:16:18 +0600

    feat: enhance OurFoodShowcase component with layout and content improvements

--- src/Components/AboutPage/OurFoodShowcase/OurFoodShowcase.jsx
+++ src/Components/AboutPage/OurFoodShowcase/OurFoodShowcase.jsx
@@ -1,8 +1,18 @@
+import Container from "../../Shared/Container/Container";
+import SectionHeader from "../../Shared/SectionHeader/SectionHeader";
 
 const OurFoodShowcase = () => {
   return (
-    <div>OurFoodShowcase</div>
-  )
-}
+    <Container>
+      <div className="flex justify-center items-center flex-col">
+        <SectionHeader heading="Our Food Showcase" subHeading="FOOD ITEMS" />
+        <p className="text-white text-center w-1/3 mx-auto mt-5">
+          Our menu is a celebration of flavors from around the world, crafted
+          with care and creativity
+        </p>
+      </div>
+    </Container>
+  );
+};
 
-export default OurFoodShowcase
+export default OurFoodShowcase;
