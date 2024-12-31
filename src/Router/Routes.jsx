4efead3b924786ed9f53661f1264688bee1a79f1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-31 23:02:17 +0600

    add contact page in routes

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import MainLayout from "../Layout/MainLayout"
 import Home from "../Pages/Home/Home"
 import About from "../Pages/About/About"
+import Contact from "../Pages/Contact/Contact"
 
 
 const Routes = createBrowserRouter([
@@ -16,6 +17,10 @@ const Routes = createBrowserRouter([
             {
                 path: "about",
                 element:<About/>
+            },
+            {
+                path: "contact",
+                element:<Contact/>
             },
         ]
     }
