e98e4926d3dc8daebce7b349396b98022a1adac2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-31 22:31:53 +0600

    update card section

--- src/Components/AboutPage/OurFoodShowcase/OurFoodShowcase.jsx
+++ src/Components/AboutPage/OurFoodShowcase/OurFoodShowcase.jsx
@@ -16,7 +16,7 @@ const OurFoodShowcase = () => {
           with care and creativity
         </p>
       </div>
-      <div>
+      <div className="grid grid-cols-4 place-items-center gap-6">
         <div className="w-full flex flex-col justify-center items-center gap-6 group relative">
           <img src={image1} alt="bengali food" className="w-full" />
           <div className="absolute flex-col  hidden group-hover:flex duration-300 transition-all justify-center items-center border-2 border-white border-dashed w-11/12 h-5/6">
