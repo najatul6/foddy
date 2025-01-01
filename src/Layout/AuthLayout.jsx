8ffa1ef42060f609436dd8e358517a3e3ebb56e4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 21:34:47 +0600

    enhance AuthLayout with centered title and Outlet for routing

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+    <div className="w-full flex justify-center items-center">
+        <div className="flex justify-center items-center flex-1">
+            <h1 className="text-3xl font-bold text-center">Auth Layout</h1>
+        </div>
+        <Outlet />
+    </div>
   )
 }
 
