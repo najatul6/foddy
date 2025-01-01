cb3bd3a71376d86456b59a6e9a8d05af0425657b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 21:41:41 +0600

    update welcome message in AuthLayout for a more inviting tone

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -5,9 +5,9 @@ const AuthLayout = () => {
     <div className="w-full flex min-h-screen">
       <div className="hidden lg:flex justify-center items-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center ">
-          <h1 className="text-5xl text-white font-bold">Welcome Back</h1>
+          <h1 className="text-5xl text-white font-bold">Welcome!</h1>
           <p className="text-gray-300">
-            To keep connected with us please login with your personal info
+            To keep connected with us with your personal info
           </p>
         </div>
       </div>
