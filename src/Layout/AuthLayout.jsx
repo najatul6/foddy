4685ab7de844114840456694c4033c4ab733a7ae
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 21:40:52 +0600

    refactor AuthLayout for improved readability and consistent formatting

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,19 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
     <div className="w-full flex min-h-screen">
-        <div className="hidden lg:flex justify-center items-center bg-black w-1/2 px-12">
+      <div className="hidden lg:flex justify-center items-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center ">
-            <h1 className="text-5xl text-white font-bold">Welcome Back</h1>
-            <p className="text-gray-300">To keep connected with us please login with your personal info</p>
+          <h1 className="text-5xl text-white font-bold">Welcome Back</h1>
+          <p className="text-gray-300">
+            To keep connected with us please login with your personal info
+          </p>
         </div>
-        </div>
-        <div className="flex-1 flex justify-center items-center px-4 py-12 sm:px-6 lg:px-8">
+      </div>
+      <div className="flex-1 flex justify-center items-center px-4 py-12 sm:px-6 lg:px-8">
         <Outlet />
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
