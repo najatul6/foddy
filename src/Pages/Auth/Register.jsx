430bff4d86c6090734619a6d46f358280305790c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 23:33:07 +0600

    feat: implement registration form layout and styling for improved user experience

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,7 +1,56 @@
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="w-full mx-auto max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Log in to your account
+        </h1>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col justify-start items-start gap-1 w-full">
+          <label className="text-lg capitalize font-medium " htmlFor="email">
+            Email Address
+          </label>
+          <input
+            className="w-full py-1 px-3 bg-transparent focus:outline-double outline-white border border-deep-orange text-deep-black placeholder:text-deep-black rounded-md"
+            placeholder="Enter your email address"
+            {...register("email", { required: "Email Address is required" })}
+            aria-invalid={errors.email ? "true" : "false"}
+          />
+          {errors.email && <p role="alert" className="text-[#a20131] w-full text-right">{errors.email.message}</p>}
+        </div>
+        <div className="flex flex-col justify-start items-start gap-1 w-full mt-3">
+          <label className="text-lg capitalize font-medium " htmlFor="password">
+            Password
+          </label>
+          <input
+            className="w-full py-1 px-3 bg-transparent focus:outline-double outline-white border border-deep-orange text-deep-black placeholder:text-deep-black rounded-md"
+            placeholder="Enter your password"
+            {...register("password", { required: "Password is required" })}
+            aria-invalid={errors.password ? "true" : "false"}
+          />
+          {errors.password && <p role="alert" className="text-[#a20131] text-right w-full">{errors.password.message}</p>}
+        </div>
+
+        <input
+          className="w-full text-lg font-bold cursor-pointer py-2 px-2 md:px-6 uppercase bg-transparent  hover:bg-deep-orange border-deep-orange text-deep-orange rounded-md hover:text-white border  duration-300  font-cormorantGaramond flex justify-center items-center gap-1 mt-6"
+          type="submit"
+        />
+      </form>
+      <hr className="w-2/3 mx-auto border-2 border-white rounded-full"/>
+      <div className="text-center">
+        <p className="">
+          Don&apos;t have an account?
+          <Link
+            to="/auth/register"
+            className="font-medium text-primary hover:underline ml-2"
+          >
+            Create Account
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
