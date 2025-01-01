2ffc0216f642fd4b0cef5cb67d5fa5ef8aa1cd95
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 22:53:48 +0600

    refactor: enhance Login component layout and form handling with improved accessibility

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,37 +1,39 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    const {
-        register,
-        formState: { errors },
-        handleSubmit,
-      } = useForm()
-      const onSubmit = (data) => console.log(data)
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>
+    <div className="w-full mx-auto">
+      <div className="max-w-md mx-auto py-10 px-4 border">
         <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("firstName", { required: true })}
-        aria-invalid={errors.firstName ? "true" : "false"}
-      />
-      {errors.firstName?.type === "required" && (
-        <p role="alert">First name is required</p>
-      )}
+          <h1 className="text-4xl font-bold text-center mb-5">Log in</h1>
+          <div className="flex flex-col justify-start items-start gap-2 w-full">
+            <label className="text-lg capitalize font-medium" htmlFor="email">Email Address</label>
+            <input
+              className="w-full py-1 px-3 bg-transparent focus:outline-dashed outline-deep-orange border border-deep-orange"
+              placeholder="Enter your email address"
+              {...register("email", { required: "Email Address is required" })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+            {errors.mail && <p role="alert">{errors.mail.message}</p>}
+          </div>
 
-      <input
-        {...register("mail", { required: "Email Address is required" })}
-        aria-invalid={errors.mail ? "true" : "false"}
-      />
-      {errors.mail && <p role="alert">{errors.mail.message}</p>}
-
-      <input type="submit" />
-    </form>
+          <input type="submit" />
+        </form>
+      </div>
     </div>
-  )
+  );
+};
+
+export default Login;
+
+{
+  /* <div className="bg-[#a20131]">
+
+</div> */
 }
-
-export default Login
-
-{/* <div className="bg-[#a20131]">
-
-</div> */}
