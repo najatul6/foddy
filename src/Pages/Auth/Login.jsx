746f0c2d004f97119c2a1244346fad2c5fe79350
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 21:57:17 +0600

    feat: implement form handling with react-hook-form in Login component

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,8 +1,31 @@
+import { useForm } from "react-hook-form"
 
 const Login = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+      } = useForm()
+      const onSubmit = (data) => console.log(data)
   return (
-    <div>Login
-       
+    <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("firstName", { required: true })}
+        aria-invalid={errors.firstName ? "true" : "false"}
+      />
+      {errors.firstName?.type === "required" && (
+        <p role="alert">First name is required</p>
+      )}
+
+      <input
+        {...register("mail", { required: "Email Address is required" })}
+        aria-invalid={errors.mail ? "true" : "false"}
+      />
+      {errors.mail && <p role="alert">{errors.mail.message}</p>}
+
+      <input type="submit" />
+    </form>
     </div>
   )
 }
