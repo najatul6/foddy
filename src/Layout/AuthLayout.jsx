7010c90e41f4879d61a81bdff0339288a0aeee5f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-01 21:38:34 +0600

    enhance AuthLayout with improved styling and welcoming message

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
   return (
-    <div className="w-full flex justify-center items-center">
-        <div className="flex justify-center items-center flex-1">
-            <h1 className="text-3xl font-bold text-center">Auth Layout</h1>
+    <div className="w-full flex min-h-screen">
+        <div className="hidden lg:flex justify-center items-center bg-black w-1/2 px-12">
+        <div className="max-w-md space-y-6 text-center ">
+            <h1 className="text-5xl text-white font-bold">Welcome Back</h1>
+            <p className="text-gray-300">To keep connected with us please login with your personal info</p>
+        </div>
         </div>
         <Outlet />
     </div>
